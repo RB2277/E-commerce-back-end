d00ce88a3c0dedffff2ee413e5e2c291d7e8861f
RB <jsriley24505@gmail.com>
2024-02-15 08:29:42 -0600

    added get all and get by id routes to tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -4,11 +4,31 @@ const { Tag, Product, ProductTag } = require('../../models');
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
+  Tag.findAll({
+    include: [
+     {
+       model: Product,
+     },
+    ]
+   },
+   ).then((tags) => {
+   res.json(tags)
+   })
   // find all tags
   // be sure to include its associated Product data
 });
 
 router.get('/:id', (req, res) => {
+  Tag.findByPk(req.params.id,{
+    include: [
+     {
+       model: Product,
+     },
+    ]
+   },
+   ).then((tags) => {
+   res.json(tags)
+   })
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
